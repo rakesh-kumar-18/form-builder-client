ab04d0a206b0fd7c755b2ebe27614bcff274b4b2
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2024-07-24 09:26:14 +0530

    added theme tab

--- src/pages/CreateTypeBotPage/CreateTypeBotPage.jsx
+++ src/pages/CreateTypeBotPage/CreateTypeBotPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./CreateTypeBotPage.module.css";
 import { MdOutlineTextsms } from "react-icons/md";
 import { CiImageOn } from "react-icons/ci";
@@ -12,24 +13,64 @@ import { CiStar } from "react-icons/ci";
 import { LuCheckSquare } from "react-icons/lu";
 import { RxCross1 } from "react-icons/rx";
 import { useNavigate } from "react-router-dom";
+import dark from "../../assets/theme-dark.png";
+import light from "../../assets/theme-light.png";
+import tailblue from "../../assets/theme-tail-blue.png";
+import profileImage from "../../assets/profile.png";
+import dot from "../../assets/dot.png";
 
 const CreateTypeBotPage = () => {
     const navigate = useNavigate();
+    const [activeTab, setActiveTab] = useState("flow");
+    const [selectedTheme, setSelectedTheme] = useState("light");
+
+    const themes = [
+        { id: "light", name: "Light", image: `${light}` },
+        { id: "dark", name: "Dark", image: `${dark}` },
+        { id: "tail-blue", name: "Tail Blue", image: `${tailblue}` },
+    ];
+
+    const handleThemeClick = (themeId) => {
+        setSelectedTheme(themeId);
+    };
 
     return (
         <div className={styles.createTypeBotPage}>
             <div className={styles.navbar}>
-                <input
-                    type="text"
-                    placeholder="Enter Form Name"
-                    className={styles.formNameInput}
-                />
+                {activeTab === "flow" ? (
+                    <input
+                        type="text"
+                        placeholder="Enter Form Name"
+                        className={styles.formNameInput}
+                    />
+                ) : (
+                    <div style={{ width: "180px" }}></div>
+                )}
                 <div className={styles.tabs}>
-                    <button className={`${styles.tab} ${styles.activeTab}`}>
+                    <button
+                        className={`${styles.tab} ${
+                            activeTab === "flow" ? styles.activeTab : ""
+                        }`}
+                        onClick={() => setActiveTab("flow")}
+                    >
                         Flow
                     </button>
-                    <button className={styles.tab}>Theme</button>
-                    <button className={styles.tab}>Response</button>
+                    <button
+                        className={`${styles.tab} ${
+                            activeTab === "theme" ? styles.activeTab : ""
+                        }`}
+                        onClick={() => setActiveTab("theme")}
+                    >
+                        Theme
+                    </button>
+                    <button
+                        className={`${styles.tab} ${
+                            activeTab === "response" ? styles.activeTab : ""
+                        }`}
+                        onClick={() => setActiveTab("response")}
+                    >
+                        Response
+                    </button>
                 </div>
                 <div className={styles.actions}>
                     <button className={styles.shareButton}>Share</button>
@@ -44,66 +85,120 @@ const CreateTypeBotPage = () => {
                     />
                 </div>
             </div>
-            <div className={styles.content}>
-                <div className={styles.sidebar}>
-                    <h5>Bubbles</h5>
-                    <div className={styles.bubbleGroup}>
-                        <button className={styles.bubble}>
-                            <MdOutlineTextsms className={styles.bubbleIcon} />
-                            Text
-                        </button>
-                        <button className={styles.bubble}>
-                            <CiImageOn className={styles.bubbleIcon} />
-                            Image
-                        </button>
-                        <button className={styles.bubble}>
-                            <TbMovie className={styles.bubbleIcon} />
-                            Video
-                        </button>
-                        <button className={styles.bubble}>
-                            <MdGif
-                                className={styles.bubbleIcon}
-                                style={{ fontSize: "x-large" }}
+            {activeTab === "flow" && (
+                <div className={styles.content}>
+                    <div className={styles.sidebar}>
+                        <h5>Bubbles</h5>
+                        <div className={styles.bubbleGroup}>
+                            <button className={styles.bubble}>
+                                <MdOutlineTextsms
+                                    className={styles.bubbleIcon}
+                                />
+                                Text
+                            </button>
+                            <button className={styles.bubble}>
+                                <CiImageOn className={styles.bubbleIcon} />
+                                Image
+                            </button>
+                            <button className={styles.bubble}>
+                                <TbMovie className={styles.bubbleIcon} />
+                                Video
+                            </button>
+                            <button className={styles.bubble}>
+                                <MdGif className={styles.bubbleIcon} />
+                                GIF
+                            </button>
+                        </div>
+                        <h5>Inputs</h5>
+                        <div className={styles.inputGroup}>
+                            <button className={styles.input}>
+                                <RxText className={styles.inputIcon} />
+                                Text
+                            </button>
+                            <button className={styles.input}>
+                                <FaHashtag className={styles.inputIcon} />
+                                Number
+                            </button>
+                            <button className={styles.input}>
+                                <FiAtSign className={styles.inputIcon} />
+                                Email
+                            </button>
+                            <button className={styles.input}>
+                                <FiPhone className={styles.inputIcon} />
+                                Phone
+                            </button>
+                            <button className={styles.input}>
+                                <CiCalendarDate className={styles.inputIcon} />
+                                Date
+                            </button>
+                            <button className={styles.input}>
+                                <CiStar className={styles.inputIcon} />
+                                Rating
+                            </button>
+                            <button className={styles.input}>
+                                <LuCheckSquare className={styles.inputIcon} />
+                                Buttons
+                            </button>
+                        </div>
+                    </div>
+                    <div className={styles.workspace}>
+                        <div className={styles.startBubble}>Start</div>
+                    </div>
+                </div>
+            )}
+            {activeTab === "theme" && (
+                <div className={styles.themeContent}>
+                    <div className={styles.themeSidebar}>
+                        <h5>Customize the theme</h5>
+                        {themes.map((theme) => (
+                            <div
+                                key={theme.id}
+                                className={styles.themeOption}
+                                onClick={() => handleThemeClick(theme.id)}
+                            >
+                                <img
+                                    src={theme.image}
+                                    alt={theme.name}
+                                    className={styles.themeImage}
+                                    style={{
+                                        border:
+                                            selectedTheme === theme.id &&
+                                            "4px solid #1A5FFF",
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                    <div
+                        className={styles.preview}
+                        style={{
+                            backgroundColor:
+                                selectedTheme === "light"
+                                    ? "#FFFFFF"
+                                    : selectedTheme === "dark"
+                                      ? "#171923"
+                                      : "#508C9B",
+                        }}
+                    >
+                        <div className={styles.botProfile}>
+                            <img
+                                src={profileImage}
+                                alt="Profile"
+                                className={styles.profileImage}
                             />
-                            GIF
-                        </button>
-                    </div>
-                    <h5>Inputs</h5>
-                    <div className={styles.inputGroup}>
-                        <button className={styles.input}>
-                            <RxText className={styles.inputIcon} />
-                            Text
-                        </button>
-                        <button className={styles.input}>
-                            <FaHashtag className={styles.inputIcon} />
-                            Number
-                        </button>
-                        <button className={styles.input}>
-                            <FiAtSign className={styles.inputIcon} />
-                            Email
-                        </button>
-                        <button className={styles.input}>
-                            <FiPhone className={styles.inputIcon} />
-                            Phone
-                        </button>
-                        <button className={styles.input}>
-                            <CiCalendarDate className={styles.inputIcon} />
-                            Date
-                        </button>
-                        <button className={styles.input}>
-                            <CiStar className={styles.inputIcon} />
-                            Rating
-                        </button>
-                        <button className={styles.input}>
-                            <LuCheckSquare className={styles.inputIcon} />
-                            Buttons
-                        </button>
+                            <div className={styles.previewBubble}>
+                                <div className={styles.previewText}>Hello</div>
+                            </div>
+                        </div>
+                        <div
+                            className={`${styles.previewBubble} ${styles.botBubble}`}
+                        >
+                            <p>Hi</p>
+                            <img src={dot} alt="dot" className={styles.dot} />
+                        </div>
                     </div>
                 </div>
-                <div className={styles.workspace}>
-                    <div className={styles.startBubble}>Start</div>
-                </div>
-            </div>
+            )}
         </div>
     );
 };
